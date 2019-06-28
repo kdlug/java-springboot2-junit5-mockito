bfa92245d71a96c406087a10fcdb76c56841a6f1
kdlug <casi@o2.pl>
2019-06-28 10:45:27 +0200

    Returning values from mockito mocks

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -2,11 +2,14 @@ package guru.springframework.sfgpetclinic.services.springdatajpa;
 
 import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -18,6 +21,20 @@ class SpecialitySDJpaServiceTest {
 
     @InjectMocks
     SpecialitySDJpaService service;
+
+    @Test
+    void findByIdTest() {
+        // Mock will return back this object
+        Speciality speciality = new Speciality();
+
+        when(specialtyRepository.findById(1l)).thenReturn(Optional.of(speciality));
+
+        Speciality foundSpecialty = service.findById(1l);
+
+        Assertions.assertThat(foundSpecialty).isNotNull();
+
+        verify(specialtyRepository).findById(1l); // checks that findById was called once
+    }
 
     @Test
     void deleteById() {
