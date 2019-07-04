b5d967089b1486658aec5a79deca98e2f94e6f68
kdlug <casi@o2.pl>
2019-07-04 19:16:00 +0200

    Adding spy

--- src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
@@ -4,9 +4,13 @@ import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.Visit;
 import guru.springframework.sfgpetclinic.services.PetService;
 import guru.springframework.sfgpetclinic.services.VisitService;
+import guru.springframework.sfgpetclinic.services.map.PetMapService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,13 +19,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class VisitControllerTest {
     @Mock
     VisitService visitService;
 
-    @Mock
-    PetService petService;
+    @Spy // @Mock
+    PetMapService petService;
 
     @InjectMocks
     VisitController visitController;
@@ -30,14 +37,43 @@ class VisitControllerTest {
     void loadPetWithVisit() {
         // given
         Map<String, Object> model = new HashMap<>();
-        Pet pet = new Pet(1l);
-        given(petService.findById(anyLong())).willReturn(pet);
+        Pet pet = new Pet(12l);
+        Pet pet3 = new Pet(3l);
+
+        petService.save(pet);
+        petService.save(pet3);
+
+        given(petService.findById(anyLong())).willCallRealMethod(); // willReturn(pet);
 
         // when
-        Visit visit = visitController.loadPetWithVisit(1l, model);
+        Visit visit = visitController.loadPetWithVisit(12l, model);
 
         // then
         assertThat(visit).isNotNull();
         assertThat(visit.getPet()).isNotNull();
+        assertThat(visit.getPet().getId()).isEqualTo(12l);
+        verify(petService, times(1)).findById(anyLong());
+    }
+
+    @Test
+    void loadPetWithVisitWithStubbibing() {
+        // given
+        Map<String, Object> model = new HashMap<>();
+        Pet pet = new Pet(12l);
+        Pet pet3 = new Pet(3l);
+
+        petService.save(pet);
+        petService.save(pet3);
+
+        given(petService.findById(anyLong())).willReturn(pet3);
+
+        // when
+        Visit visit = visitController.loadPetWithVisit(12l, model);
+
+        // then
+        assertThat(visit).isNotNull();
+        assertThat(visit.getPet()).isNotNull();
+        assertThat(visit.getPet().getId()).isNotEqualTo(12l); // we can't load pet with id 12 because we are overriding real method, instead of calling it
+        verify(petService, times(1)).findById(anyLong());
     }
 }
